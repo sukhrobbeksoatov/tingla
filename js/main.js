033f406582bb45c276eaa407c3f3dfa3095c193b
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-30 03:11:04 +0500

    add changes and fix bugs

--- js/main.js
+++ js/main.js
@@ -16,3 +16,34 @@
 //     elHeader.style.backDropFilter = "none";
 //   }
 // });
+
+const video = document.querySelector("[data-player-video]");
+
+function onVideoToggleClick(evt) {
+  const el = evt.target.closest("[data-video-toggler]");
+
+  if (!el) return;
+
+  if (video.paused) {
+    video.play();
+    el.classList.add("player__toggler--pause");
+  } else {
+    video.pause();
+    el.classList.remove("player__toggler--pause");
+  }
+}
+
+document.addEventListener("click", onVideoToggleClick);
+video.addEventListener("ended", () => {
+  video.nextElementSibling.classList.remove("player__toggler--pause");
+});
+
+if (window.innerWidth > 768) {
+  video.addEventListener("mouseover", () => {
+    video.nextElementSibling.classList.remove("hidden");
+  });
+
+  video.addEventListener("mouseout", () => {
+    video.nextElementSibling.classList.add("hidden");
+  });
+}
