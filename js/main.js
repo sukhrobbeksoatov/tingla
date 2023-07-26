e332a83dcb7d5992aa00c469cbd05dbdf7e6c133
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-27 01:40:49 +0500

    Delete unnecessary scripts

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// var elHeader = document.querySelector(".main-header");
-
-// elHeader.style.backgroundColor = "transparent";
-// window.addEventListener("scroll", function () {
-//   if (this.pageYOffset >= 100) {
-//     elHeader.style.position = "sticky";
-//     elHeader.style.top = "0";
-//     elHeader.style.paddingTop = "30px";
-//     elHeader.style.paddingBottom = "30px";
-//     elHeader.style.backgroundColor = "rgba(255,255,255, 0.87)";
-//     elHeader.style.backDropFilter = "blur(15px)";
-//   } else {
-//     elHeader.style.paddingTop = "60px";
-//     elHeader.style.paddingBottom = "60px";
-//     elHeader.style.BackgroundColor = "transparent";
-//     elHeader.style.backDropFilter = "none";
-//   }
-// });
-
 const video = document.querySelector("[data-player-video]");
 
 function onVideoToggleClick(evt) {
